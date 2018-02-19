f9e99a02dab0869826147a4e7c7a041dd53333d3
Justin Chiu <justinchiu@g.harvard.edu>
2018-02-19 17:22:26 -0500

    separate language directions

--- scripts/train_master.sh
+++ scripts/train_master.sh
@@ -1,62 +1,68 @@
 ROOT=/n/rush_lab/data/wmt14-de-en
 TEXT=${ROOT}/data/wmt14_en_de
 
-DATA=$ROOT/data-onmt-master/wmt14_en_de.150-150.3-3
+DEENDATA=$ROOT/data-onmt-master/wmt14_de_en.150-150.3-3
+ENDEDATA=$ROOT/data-onmt-master/wmt14_en_de.150-150.3-3
 
-MODEL=/n/rush_lab/jc/onmt-master/wmt14-de-en/models
-LOG=/n/rush_lab/jc/onmt-master/wmt14-de-en/logs
-GEN=/n/rush_lab/jc/onmt-master/wmt14-de-en/gen
+DEENMODEL=/n/rush_lab/jc/onmt-master/wmt14-de-en/models
+DEENLOG=/n/rush_lab/jc/onmt-master/wmt14-de-en/logs
+DEENGEN=/n/rush_lab/jc/onmt-master/wmt14-de-en/gen
+
+ENDEMODEL=/n/rush_lab/jc/onmt-master/wmt14-en-de/models
+ENDELOG=/n/rush_lab/jc/onmt-master/wmt14-en-de/logs
+ENDEGEN=/n/rush_lab/jc/onmt-master/wmt14-en-de/gen
 
 train_wmt14deen_master_baseline_brnn() {
     seed=$(od -A n -t d -N 1 /dev/urandom | tr -d ' ')
-    name=baseline.brnn.s$seed
-    mkdir -p $MODEL/$name
-    mkdir -p $LOG
+    name=de2en.baseline.brnn.s$seed
+    mkdir -p $DEENMODEL/$name
+    mkdir -p $DEENLOG
     python /n/home13/jchiu/python/OpenNMT-py/train.py \
         -encoder_type brnn \
-        -data $DATA \
-        -save_model $MODEL/$name/$name \
+        -data $DEENDATA \
+        -save_model $DEENMODEL/$name/$name \
         -seed $seed \
         -epochs 30 \
         -gpuid $1 \
         -batch_size 256 \
-        | tee ${LOG}/$name.log
+        | tee ${DEENLOG}/$name.log
 }
 
-generate_wmtdeen_master_baseline_brnn() {
-    mkdir -p $GEN
+generate_wmt14deen_master_baseline_brnn() {
+    # old naming convetion, but it's fine.
+    mkdir -p $DEENGEN
     name=baseline.brnn.s237
     python /n/home13/jchiu/python/OpenNMT-py/translate.py \
-        -model $MODEL/$name/${name}_acc_63.81_ppl_6.37_e30.pt \
+        -model $DEENMODEL/$name/${name}_acc_63.81_ppl_6.37_e30.pt \
         -src $TEXT/test.de \
-        -output $GEN/$name.test.en \
+        -output $DEENGEN/$name.test.en \
         -gpu $1 \
         -beam_size 5
 }
 
-train_wmt14deen_master_baseline_brnn() {
+train_wmt14ende_master_baseline_brnn() {
     seed=$(od -A n -t d -N 1 /dev/urandom | tr -d ' ')
-    name=baseline.brnn.s$seed
-    mkdir -p $MODEL/$name
-    mkdir -p $LOG
+    name=en2de.baseline.brnn.s$seed
+    mkdir -p $ENDEMODEL/$name
+    mkdir -p $ENDELOG
     python /n/home13/jchiu/python/OpenNMT-py/train.py \
         -encoder_type brnn \
-        -data $DATA \
-        -save_model $MODEL/$name/$name \
+        -data $ENDEDATA \
+        -save_model $ENDEMODEL/$name/$name \
         -seed $seed \
         -epochs 30 \
         -gpuid $1 \
         -batch_size 256 \
-        | tee ${LOG}/$name.log
+        | tee ${ENDELOG}/$name.log
 }
 
-generate_wmtdeen_master_baseline_brnn() {
-    mkdir -p $GEN
-    name=baseline.brnn.s237
+generate_wmt14ende_master_baseline_brnn() {
+    mkdir -p $ENDEGEN
+    name=en2de.baseline.brnn.s237
     python /n/home13/jchiu/python/OpenNMT-py/translate.py \
-        -model $MODEL/$name/${name}_acc_63.81_ppl_6.37_e30.pt \
+        -model $ENDEMODEL/$name/${name}_acc_63.81_ppl_6.37_e30.pt.nonexistent \
         -src $TEXT/test.de \
-        -output $GEN/$name.test.en \
+        -output $ENDEGEN/$name.test.en \
         -gpu $1 \
         -beam_size 5
 }
